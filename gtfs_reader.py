53bbed927239388ba1002dd0dcf4338e384a2d75
Belics Fanni <fanni.belics@gmail.com>
2023-09-30 20:42:27 +0200

    Updating reader

--- gtfs_reader.py
+++ gtfs_reader.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from pymongo import MongoClient
+from pymongo import MongoClient as mongo
 import os
 from graph_elements.node import Node
+from dotenv import load_dotenv, find_dotenv
+import os
 import csv
 
 absolute_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-gtfs_path = "gtfs-debrecen"
+load_dotenv(find_dotenv())
+gtfs_path = os.environ.get("READ_DICTIONARY")
 full_path = os.path.join(absolute_path, gtfs_path)
 
 
 #with open (full_path + "/agency.txt", encoding="utf-8") as ageny_txt:
 #    print(ageny_txt.read())
 
-with open (full_path + "/stops.txt", encoding="utf-8") as stops_txt:
-    nodes = set()
-    for stop in csv.reader(stops_txt):
-       if stop[0] != "id":
-           newNode = Node(stop[0],stop[1],stop[7],stop[6])
-           nodes.add(newNode)
-           print(newNode)
+def read_nodes() -> set(Node):
+    with open (full_path + "/stops.txt", encoding="utf-8") as stops_txt:
+        nodes = set()
+        for stop in csv.reader(stops_txt):
+            if stop[0] != "id":
+                
+                newNode = Node(stop[0],stop[1],stop[7],stop[6])
+                nodes.add(newNode)
+                print(newNode)
+                
