64537c990315e363c170b372b6b347e4642c37e9
Belics Fanni <fanni.belics@gmail.com>
2023-10-25 16:32:33 +0200

    new database functions designed for nodes

--- database_management/database_functions.py
+++ database_management/database_functions.py
@@ -20,18 +20,47 @@ client = MongoClient(connection_string)
 
 database = client[DICTIONARY]
 
-def create_dictionary_proper_types(node: Node) -> dict:
+def to_dictionary(node: Node) -> dict:
     data = {
             "gtfs-id": int(node.gtfs_id),
             "name": node.name,
+            "short-name": node.short_name,
             "description": node.description,
             "longitude" : float(node.longitude),
             "latitude" : float(node.latitude)
         }
+    if node.parental_node != None: 
+        data.update({"parental-node" : int(node.parental_node)})
+        
+    if len(node.children) > 0:
+        li = list(node.children)
+        data.update({"children" : li})
+        
     return data
 
+def to_node(candidate: dict) -> Node:
+    node = Node()
+    node.gtfs_id = candidate["gtfs-id"]
+    node.name = candidate["name"]
+    node.short_name = candidate["short-name"]
+    node.description = candidate["description"]
+    node.longitude = candidate["longitude"]
+    node.latitude = candidate["latitude"]
+    if "parental-node" in candidate.keys:
+        node.parental_node = candidate["parental-node"]
+    else:
+        node.parental_node = None
+        
+    if "children" in candidate.keys:
+        node.children = candidate["children"]
+    else:
+        node.children = []
+        
+    return node
+    
+
 def upload_node_to_database(node: Node) -> bool:
-    data = create_dictionary_proper_types(node)
+    data = to_dictionary(node)
     try:
         database[NODES_COLLECTION].insert_one(data)
         
@@ -42,4 +71,19 @@ def upload_node_to_database(node: Node) -> bool:
     else:
         return True
 
+def add_parental_node_to_node(node: Node, parent: int):
+    try:
+        database[NODES_COLLECTION].update_one({"gtfs-id" : int(node.gtfs_id)},
+                                              {"$set": {"parental-node" : int(parent)}})
+    except Exception as e:
+        print(e)
     
+def add_child_to_node(node: Node, child: int): 
+    try:
+        database[NODES_COLLECTION].update_one({"gtfs-id" : int(node.gtfs_id)},
+                                              {"$push" : {"children" : child}})
+    except Exception as e:
+        print(e)
+
+def find_node_by_gtfs_id(gtfs_id: int) -> Node: 
+    return database[NODES_COLLECTION].find_one({'gtfs-id': gtfs_id})
